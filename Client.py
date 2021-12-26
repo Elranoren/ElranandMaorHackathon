04d82710cba9b7dd3eec32d07dd8b9e6e1e6a106
Maor <bitondor@post.bgu.ac.il>
2021-12-26 20:44:34 +0200

    adding server and client

---
+++ Client.py
@@ -0,0 +1,30 @@
+import struct
+from socket import *
+
+
+class Client:
+    def __init__(self, team_name):
+        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.udp_client_socket = socket(AF_INET, SOCK_DGRAM)
+        self.my_ip = "0.0.0.0"  # TODO change to scapy.get_if_addr('eth1')
+        self.team_name = team_name
+
+    def looking_for_server(self):
+        self.udp_client_socket.bind((self.my_ip, 5400))
+        while True:
+            # try:
+            print("Client started, listening for offer requests...")
+            packed_message, address = self.udp_client_socket.recvfrom(4096)
+            magic_cookie, message_type, server_port = struct.unpack('IBH', packed_message)
+            if hex(int(magic_cookie)) != hex(2882395322) or int(message_type) != 2:
+                return  # TODO think what to to do
+            self.connected_to_server(address, server_port)
+            # except Exception as e:
+            #     print(e)
+
+    def connected_to_server(self, address, server_port):
+        print("Received offer from " + address + ", attempting to connect...")
+        self.tcp_socket.connect((address, server_port))
+
+client = Client("Snoku")
+client.looking_for_server()
--- Server.py
+++ Server.py
@@ -2,13 +2,36 @@ import random
 from socket import *
 import struct
 import scapy
-cs = socket(AF_INET, SOCK_DGRAM)
-cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-UDP_IP = '255.255.255.255'
-UDP_PORT = 13117
-magic_cookie = int(0xabcddcba)
-server_port = random.Random().randint(1024, 65535)
-message_type = int(0x2)
-message = struct.pack('IHB', magic_cookie, server_port, message_type)
-cs.sendto(message, (UDP_IP, UDP_PORT))
+class Server:
+
+    def __init__(self):
+        self.udp_socket = socket(AF_INET, SOCK_DGRAM)
+        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.udp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        self.UDP_IP = '255.255.255.255'
+        self.UDP_PORT = 13117
+        magic_cookie = int(0xabcddcba)
+        # self.server_port = random.Random().randint(1024, 65535)
+        self.server_port = 5400
+        message_type = int(0x2)
+        self.message = struct.pack('IBH', magic_cookie, message_type, self.server_port)  # TODO check why 8 byte instead of 7
+        self.my_ip = "127.0.0.1" # TODO change to scapy.get_if_addr('eth1')
+
+    def broadcast_message(self):
+        self.tcp_socket.bind((self.my_ip, self.server_port))  # TODO check if it is correct
+        self.tcp_socket.settimeout(1)
+        self.tcp_socket.listen(1)
+        print("Server started, listening on IP address " + self.my_ip)
+        self.udp_socket.sendto(self.message, (self.UDP_IP, self.UDP_PORT))
+        while True:
+            try:
+                # print("before")
+                #accept(1 sec)
+                client_socket, address = self.tcp_socket.accept()
+                print("after")
+            except:
+                self.udp_socket.sendto(self.message, (self.UDP_IP, self.UDP_PORT))
+
+server = Server()
+server.broadcast_message()

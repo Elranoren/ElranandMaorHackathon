89f875b62a3399b89172625029a14a69a7ccba41
Maor <bitondor@post.bgu.ac.il>
2021-12-26 21:56:56 +0200

    adding server and client

--- Client.py
+++ Client.py
@@ -5,26 +5,34 @@ from socket import *
 class Client:
     def __init__(self, team_name):
         self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.udp_client_socket = socket(AF_INET, SOCK_DGRAM)
+        self.udp_client_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.my_ip = "0.0.0.0"  # TODO change to scapy.get_if_addr('eth1')
         self.team_name = team_name
 
     def looking_for_server(self):
         self.udp_client_socket.bind(('', 13117))
-        while True:
-            # try:
+        # while True:
+        try:
             print("Client started, listening for offer requests...")
             packed_message, address = self.udp_client_socket.recvfrom(4096)
             magic_cookie, message_type, server_port = struct.unpack('>IBH', packed_message)
             if hex(int(magic_cookie)) != hex(2882395322) or int(message_type) != 2:
                 return  # TODO think what to to do
             self.connected_to_server(address[0], server_port)
-            # except Exception as e:
-            #     print(e)
+        except Exception as e:
+            pass
 
     def connected_to_server(self, address, server_port):
         print("Received offer from " + address + ", attempting to connect...")
-        self.tcp_socket.connect((address, server_port))
+        self.tcp_socket.connect(("127.0.0.1", server_port))
+        team_name_bytes = str.encode(self.team_name+"\n")
+        self.tcp_socket.send(team_name_bytes)
 
 client = Client("Snoku")
+client1 = Client("Snoku1")
+client2 = Client("Snoku2")
 client.looking_for_server()
+client1.looking_for_server()
+client2.looking_for_server()
--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import random
+import time
 from socket import *
 import struct
 import scapy
@@ -21,17 +22,33 @@ class Server:
     def broadcast_message(self):
         self.tcp_socket.bind((self.my_ip, self.server_port))  # TODO check if it is correct
         self.tcp_socket.settimeout(1)
-        self.tcp_socket.listen(1)
+        self.tcp_socket.listen(0)
         print("Server started, listening on IP address " + self.my_ip)
         self.udp_socket.sendto(self.message, (self.UDP_IP, self.UDP_PORT))
-        while True:
+        self.connection_players()
+
+    def connection_players(self):
+        connected_clients = []
+        while len(connected_clients) < 2:
             try:
                 # print("before")
-                #accept(1 sec)
+                # accept(1 sec)
                 client_socket, address = self.tcp_socket.accept()
+                team_name_bytes = client_socket.recv(1024)
+                team_name = team_name_bytes.decode()
+                new_client = (client_socket, address, team_name)
+                print(new_client)
+                connected_clients.append(new_client)
                 print("after")
             except:
                 self.udp_socket.sendto(self.message, (self.UDP_IP, self.UDP_PORT))
+        print("out of while")
+        self.start_game(connected_clients[:2])
+
+    def start_game(self,connected_clients):
+        time.sleep(10)
+        print("Game Starts")
+
 
 server = Server()
 server.broadcast_message()

2a5fd8fe1a809d2f65fbda65fec73fe84e059bc2
Maor <bitondor@post.bgu.ac.il>
2021-12-29 17:07:21 +0200

    adding new feature to the Server lik score board and best team and fixing the bug that the client and server crushed

--- Client.py
+++ Client.py
@@ -1,13 +1,9 @@
-
 import struct
 import threading
 import time
 from multiprocessing import Process
 from socket import *
 import getch
-# import keyword
-
-# from pynput import keyboard
 
 
 class Client:
@@ -16,18 +12,20 @@ class Client:
         self.tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.udp_client_socket = socket(AF_INET, SOCK_DGRAM)
         self.udp_client_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.my_ip = "0.0.0.0"  # TODO change to scapy.get_if_addr('eth1')
         self.team_name = team_name
+        self.udp_client_socket.bind(('', 13117))
 
     def looking_for_server(self):
-        self.udp_client_socket.bind(('', 13117))
+
+        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         while True:
             try:
                 print("Client started, listening for offer requests...")
                 packed_message, address = self.udp_client_socket.recvfrom(4096)
                 magic_cookie, message_type, server_port = struct.unpack('>IBH', packed_message)
-                if address[0]!='172.18.0.146' or hex(int(magic_cookie)) != hex(2882395322) or int(message_type) != 2:
-                    continue  # TODO think what to to do address[0]!='172.18.0.146' or
+                # if address[0]!='172.18.0.146' or hex(int(magic_cookie)) != hex(2882395322) or int(message_type) != 2:
+                #     continue  # TODO think what to to do address[0]!='172.18.0.146' or
                 self.connected_to_server(address[0], server_port)
             except Exception as e:
                 # print(e)
@@ -36,7 +34,7 @@ class Client:
     def connected_to_server(self, address, server_port):
         print("Received offer from " + address + ", attempting to connect...")
         self.tcp_socket.connect((address, server_port))
-        team_name_bytes = str.encode(self.team_name+"\n")
+        team_name_bytes = str.encode(self.team_name + "\n")
         self.tcp_socket.send(team_name_bytes)
         self.game_mode()
 
@@ -44,12 +42,6 @@ class Client:
         question_bytes = self.tcp_socket.recv(1024)
         question = question_bytes.decode()
         print(question)
-        # client_answer = keyboard.Listener(on_press=self.on_press)
-        # client_answer.start()  # start to listen on a separate thread
-        # client_answer.join()
-        # client_answer = getch.getch()
-        # client_answer_bytes = str.encode(client_answer)
-        # self.tcp_socket.send(client_answer_bytes)
         getch_thread = Process(target=self.getch_handler)
         getch_thread.start()
         server_result_bytes = self.tcp_socket.recv(1024)
@@ -59,59 +51,13 @@ class Client:
         self.tcp_socket.close()
         self.tcp_socket = socket(AF_INET, SOCK_STREAM)
         self.tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        print("Server disconnected, listening for offer requests...")
         self.looking_for_server()
-    
+
     def getch_handler(self):
         client_answer = getch.getch()
         client_answer_bytes = str.encode(client_answer)
         self.tcp_socket.send(client_answer_bytes)
-        
 
-    # def our_getch(self):
-    #     import termios
-    #     import sys, tty
-    #     def _getch():
-    #         fd = sys.stdin.fileno()
-    #         old_settings = termios.tcgetattr(fd)
-    #         try:
-    #             tty.setraw(fd)
-    #             ch = sys.stdin.read(1)
-    #         finally:
-    #             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    #         return ch
 
-    #     return _getch()
-
-    # def on_press(key):
-    #     if key in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:  # keys of interest
-    #         # self.keys.append(k)  # store it in global-like variable
-    #         print('Key pressed: ' + key)
-    #         return False  # stop listener; remove this if want more keys
-# c1 = Client("Snoku")
-# c2 = Client("Snoku1")
-# p1 = Process(target=c1.looking_for_server).start()
-# time.sleep(2)
-# p2 = Process(target=c2.looking_for_server).start()
-# p1.join()
-# p2.join()
-# GOOD down!
-# c1 = Client1("Snoku")
-# c2 = Client1("Snoku1")
-# c3 = Client("Snoku2")
-# p1 = threading.Thread(target=c1.looking_for_server)
-# p1.start()
-# p2 = threading.Thread(target=c2.looking_for_server)
-# p2.start()
-# p3 = threading.Thread(target=c3.looking_for_server)
-# p3.start()
-# p1.join()
-# p2.join()
-# p3.join()
-
-# GOOD Up!
-# client1 = Client("Snoku1")
-# client2 = Client("Snoku2")
-# client.looking_for_server()
-# client1.looking_for_server()
-# client2.looking_for_server()
-Client("Snoku").looking_for_server()
+Client("SchnitzelBehala").looking_for_server()
--- Server.py
+++ Server.py
@@ -10,13 +10,15 @@ from scapy.arch import get_if_addr
 class Server:
 
     def __init__(self):
-        self.question_dict = {"How much is 1+1?":2, "How much is 1+2?":3, "How much is 1+3?":4, "How much is 1+5?":6, "How much is 2+3?":5}
-        # for i in range(10):
-        #     for j in range(10):
-        #         if i + j < 10:
-        #             temp_question = f"How much is {i}+{j}?"
-        #             if temp_question not in self.question_dict:
-        #                 self.question_dict[temp_question] = i + j
+        self.score_board = {}
+        self.best_team = ""
+        self.question_dict = {}
+        for i in range(10):
+            for j in range(10):
+                if i + j < 10:
+                    temp_question = f"How much is {i}+{j}?"
+                    if temp_question not in self.question_dict:
+                        self.question_dict[temp_question] = i + j
         self.udp_socket = socket(AF_INET, SOCK_DGRAM)
         self.tcp_socket = socket(AF_INET, SOCK_STREAM)
         self.udp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -30,7 +32,7 @@ class Server:
         message_type = int(0x2)
         self.message = struct.pack('>IBH', magic_cookie, message_type,
                                    self.server_port)
-        self.my_ip =  get_if_addr("eth1")
+        self.my_ip = get_if_addr("eth1")
         self.was_answered = False
         self.lock = threading.Lock()
         self.winning_team = ""
@@ -50,7 +52,14 @@ class Server:
                 # print("before")
                 # accept(1 sec)
                 client_socket, address = self.tcp_socket.accept()
-                # print("after")
+                # if address[0] != '172.18.0.14':
+                #     continue
+                # if len(connected_clients) == 1:
+                #     try:
+                #         ready_to_read, ready_to_write, in_error = select([connected_clients[0][0],], [connected_clients[0][0],], [], 2)
+                #     except:
+                #         connected_clients.pop(0)
+                print("after")
                 team_name_bytes = client_socket.recv(1024)
                 team_name = team_name_bytes.decode()
                 new_client = (client_socket, address, team_name)
@@ -63,41 +72,57 @@ class Server:
         self.start_game(connected_clients[:2])
 
     def start_game(self, connected_clients):
-        question_keys = list(self.question_dict.keys())
-        index = random.randint(0, len(question_keys) - 1)
-        question = question_keys[index]
-        answer = self.question_dict[question]
-        self.winning_team = ""
-        self.was_answered = False
-        # time.sleep(10) TODO
-        welcome_message = f"""Welcome to Quick Maths.
+        try:
+            question_keys = list(self.question_dict.keys())
+            index = random.randint(0, len(question_keys) - 1)
+            question = question_keys[index]
+            answer = self.question_dict[question]
+            self.winning_team = ""
+            self.was_answered = False
+            # time.sleep(10)
+            welcome_message = f"""Welcome to Quick Maths.
 Player 1: {connected_clients[0][2]}Player 2: {connected_clients[1][2]}==
 Please answer the following question as fast as you can:
 {question}"""
-        print(welcome_message)
-        thread1 = threading.Thread(target=self.handle_client, args=(
-        connected_clients[0][0], connected_clients[0][1], connected_clients[0][2], welcome_message, answer,connected_clients[1][2]))
-        thread2 = threading.Thread(target=self.handle_client, args=(
-        connected_clients[1][0], connected_clients[1][1], connected_clients[1][2], welcome_message, answer,connected_clients[0][2]))
-        thread1.start()
-        thread2.start()
-        # thread1.join()
-        # thread2.join()
-        while thread1.is_alive() and thread2.is_alive():
-            time.sleep(0.1)
-        # recive result from one of the clients and then:
-        summary_message = f"""Game over!
-        The correct answer was {answer}!
-        
-        Congratulations to the winner: {self.winning_team}"""
-        summary_message_bytes = str.encode(summary_message)
-        for client_socket, address, team_name in connected_clients:
-            client_socket.sendall(summary_message_bytes)
-            # client_socket.close()
-        print("Game over, sending out offer requests...")
-        self.broadcast_message()
+            print(welcome_message)
+            thread1 = threading.Thread(target=self.handle_client, args=(
+                connected_clients[0][0], connected_clients[0][1], connected_clients[0][2], welcome_message, answer,
+                connected_clients[1][2]))
+            thread2 = threading.Thread(target=self.handle_client, args=(
+                connected_clients[1][0], connected_clients[1][1], connected_clients[1][2], welcome_message, answer,
+                connected_clients[0][2]))
+            thread1.start()
+            thread2.start()
+            # thread1.join()
+            # thread2.join()
+            while thread1.is_alive() and thread2.is_alive():
+                time.sleep(0.1)
+            # recive result from one of the clients and then:
+            summary_message = f"""Game over!
+            The correct answer was {answer}!
 
-    def handle_client(self, client_socket, address, my_team_name, welcome_message, answer,their_team_name):
+            Congratulations to the winner: {self.winning_team}"""
+            if self.winning_team != "Draw" and self.winning_team != "":
+                win_tem = self.winning_team.strip("\n")
+                if win_tem in self.score_board:
+                    self.score_board[win_tem] += 1
+                else:
+                    self.score_board[win_tem] = 1
+                print(self.score_board)
+            if len(self.score_board) > 0:
+                self.best_team = max(self.score_board, key=self.score_board.get)
+                print("The best team so far is: " + self.best_team)
+            summary_message_bytes = str.encode(summary_message)
+            for client_socket, address, team_name in connected_clients:
+                client_socket.sendall(summary_message_bytes)
+                # client_socket.close()
+            print("Game over, sending out offer requests...")
+            self.broadcast_message()
+        except Exception as e:
+            print(e)
+            self.broadcast_message()
+
+    def handle_client(self, client_socket, address, my_team_name, welcome_message, answer, their_team_name):
         try:
             question_bytes = str.encode(welcome_message)
             client_socket.sendall(question_bytes)
